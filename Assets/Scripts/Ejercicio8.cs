d54d542cae792e48d430e73268b66d27c90ffdec
juanifogo <juanifogo@gmail.com>
2022-03-31 15:05:35 -0300

    ejercicio 8 y 9

---
+++ Assets/Scripts/Ejercicio8.cs
@@ -0,0 +1,36 @@
+﻿//8. Crear una variable entera que pueda ser modificada desde el Inspector.Suponiendo que
+//el primer día de la semana es el Domingo, mostrar el nombre del día correspondiente al
+//valor de la variable “dia”. Si el día no está en el rango permitido debe emitir por pantalla el
+//mensaje de error &quot;El día ingresado no es válido&quot;.
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ejercicio8 : MonoBehaviour
+{
+    public int dia;
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (dia<1 || dia>7)
+        {
+            Debug.Log("El dia ingresado no es valido");
+        }
+        switch (dia)
+        {
+            case 1: Debug.Log("Domingo"); break;
+            case 2: Debug.Log("Lunes"); break;
+            case 3: Debug.Log("Martes"); break;
+            case 4: Debug.Log("Miercoles"); break;
+            case 5: Debug.Log("Jueves"); break;
+            case 6: Debug.Log("Viernes"); break;
+            case 7: Debug.Log("Sabado"); break;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/Ejercicio9.cs
@@ -0,0 +1,34 @@
+﻿//9. Realizá un programa que resuelva el siguiente problema:
+//Tres personas aportan diferente capital a una sociedad.Se desea saber qué porcentaje del
+//total aportó cada una (indicando nombre y porcentaje) y cuál es el monto del total aportado
+//por las tres.
+//Ingresar por Inspector el ingreso del capital aportado por cada una de las personas y luego
+//mostrar lo pedido en el siguiente formato:
+//Nombre: capital aportado: $ … , Porcentaje del capital: %.... , Monto total aportado: $ ….
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ejercicio9 : MonoBehaviour
+{
+    public float cant1, cant2, cant3;
+    public string nom1, nom2, nom3;
+    // Start is called before the first frame update
+    void Start()
+    {
+        float prcnt1, prcnt2, prcnt3, total;
+        total = cant1 + cant2 + cant3;
+        prcnt1 = 100 * cant1 / total;
+        prcnt2 = 100 * cant2 / total;
+        prcnt3 = 100 * cant3 / total;
+        Debug.Log("Nombre: " + nom1 + " // Capital aportado: $" + cant1 + " // Porcentaje del capital: %" + prcnt1 + " // Monto total aportado: " + total);
+        Debug.Log("Nombre: " + nom2 + " // Capital aportado: $" + cant2 + " // Porcentaje del capital: %" + prcnt2 + " // Monto total aportado: " + total);
+        Debug.Log("Nombre: " + nom3 + " // Capital aportado: $" + cant3 + " // Porcentaje del capital: %" + prcnt3 + " // Monto total aportado: " + total);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}

85f67abace422422eafc0b9c5c72bd3968c32a14
Juan Ignacio Fogolin <83310328+juanifogo@users.noreply.github.com>
2022-04-08 22:10:52 -0300

    ejercicio 12 hecho

--- Assets/Scripts/Ejercicio11.cs
+++ Assets/Scripts/Ejercicio11.cs
@@ -1,13 +1,33 @@
-﻿using System.Collections;
+﻿//11. Realizá un programa que resuelva el siguiente problema:
+//Deberá ingresarse por Inspector una fecha de compra(String en el siguiente formato:
+//YYYYMMDD), un nombre de comprador, un nombre de producto y una cantidad y precio del
+//producto comprado.Mostrá a modo de ticket, la información ingresada y el monto a pagar.
+//Modelo de Ticket:
+//Fecha de Compra: YYYYMMDD
+//Nombre del Comprador: xxxxx xxxxx
+//Producto solicitado: xxxxx
+//Cantidad solicitada: xx
+//Precio Unitario: $xxx
+//Total a Pagar: $xxxxx
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Ejercicio11 : MonoBehaviour
 {
+    public int Cant;
+    public float PrecioUni;
+    public string Producto, NomComprador, FechaCompra;
     // Start is called before the first frame update
     void Start()
     {
-        
+        float Total = PrecioUni * Cant;
+        Debug.Log("Fecha de compra: " + FechaCompra);
+        Debug.Log("Nombre del comprador:" + NomComprador);
+        Debug.Log("Producto solicitado: " + Producto);
+        Debug.Log("Cantidad solicitada: " + Cant);
+        Debug.Log("Precio unitario " + PrecioUni);
+        Debug.Log("Total a pagar: " + Total);
     }
 
     // Update is called once per frame
--- Assets/Scripts/Ejercicio12.cs
+++ Assets/Scripts/Ejercicio12.cs
@@ -1,13 +1,52 @@
-﻿using System.Collections;
+﻿//12. En un casino de juegos se desea mostrar los mensajes respectivos por el puntaje
+//obtenido en el lanzamiento de tres dados de un cliente, de acuerdo a los siguientes
+//resultados:
+//• Si los tres dados son seis, mostrar el mensaje “Excelente”
+//• Si dos dados son seis, mostrar el mensaje “Muy bien”
+//• Si un dado es seis, mostrar el mensaje “Regular”
+//• Si ningún dado se obtiene seis, mostrar el mensaje “Insuficiente”
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Ejercicio12 : MonoBehaviour
 {
+    byte CantDe6;
+    string resultado;
+    public int dado1, dado2, dado3;
     // Start is called before the first frame update
     void Start()
     {
-        
+
+        if (dado1 < 1 || dado1 > 6 || dado2 < 1 || dado2 > 6 || dado3 < 1 || dado3 > 6)
+        {
+            Debug.Log("Valores de dados no validos");
+            return;
+        }
+        else if (dado1 + dado2 + dado3 == 18)
+        {
+            CantDe6 = 3;
+        }
+        else if(dado1+dado2==12 || dado1+dado3==012 || dado2+dado3==12)
+        {
+            CantDe6 = 2;
+        }
+        else if(dado1+dado2+dado3<16)
+        {
+            CantDe6 = 0;
+        }
+        else
+        {
+            CantDe6 = 1;
+        }
+        switch(CantDe6)
+        {
+            case 0: resultado = "Insuficiente"; break;
+            case 1: resultado = "Regular"; break;
+            case 2: resultado = "Muy bien"; break;
+            case 3: resultado = "Excelente"; break;
+        }
+            Debug.Log(resultado);     
     }
 
     // Update is called once per frame
